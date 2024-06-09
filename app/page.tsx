cd01fce345a70e8327885079a984c5f484d0bba0
oktayudha05 <oktayudha05@gmail.com>
2024-06-09 17:24:02 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Carousel from "@/components/home/Carousel";
+
+export default function Home() {
+  return (
+    <>
+    <div className="h-screen">
+      <Carousel />
+    </div>
+    </>
+  );
+}
