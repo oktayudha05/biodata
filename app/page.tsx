508bd0a265001c4f61cb8d11e0e51a49cbc38d7a
oktayudha05 <oktayudha05@gmail.com>
2024-06-14 11:50:17 +0700

    seccond commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Carousel from "@/components/home/Carousel";
-
 export default function Home() {
   return (
     <>
-    <div className="h-screen">
-      <Carousel />
+    <div className="h-screen text-center align-center justify-center grid grid-row-2">
+      <div>
+      </div>
+      <div className="">
+        <span className="font-bold text-4xl drop-shadow-xl sm:text-6xl md:text-8xl text-teal-500">Teknologi Informasi</span>
+      </div>
     </div>
     </>
   );
